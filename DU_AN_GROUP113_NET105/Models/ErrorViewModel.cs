a1d2b9fb0dcfd9d6f65b2748f79d703b9eeb4a93
Sir_Giap <140328785+mrgiap736@users.noreply.github.com>
2024-05-09 22:43:30 +0700

    Add project files.

---
+++ DU_AN_GROUP113_NET105/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DU_AN_GROUP113_NET105.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
