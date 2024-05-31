bed76b83d99d973a7b2cfd6dbceea25bec8780f9
Sir_Giap <140328785+mrgiap736@users.noreply.github.com>
2024-05-31 10:19:11 +0700

    Demo cart

--- DU_AN_GROUP113_NET105/Areas/Customer/Controllers/CartCustomerController.cs
+++ DU_AN_GROUP113_NET105/Areas/Customer/Controllers/CartCustomerController.cs
@@ -1,19 +1,52 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DU_AN_GROUP113_NET105.Models.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace DU_AN_GROUP113_NET105.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class CartCustomerController : Controller
     {
+        ProjectContext _context;
+
+        public CartCustomerController(ProjectContext context)
+        {
+             _context = context;
+        }
         // GET: CartCustomerController
         public ActionResult Index()
         {
-            return View();
+            string getCartId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if(getCartId == null)
+            {
+                return RedirectToAction("Index","HomeCustomer");
+            }
+            else
+            {
+                Guid cartId = Guid.Parse(getCartId);
+
+                var cartDetails = _context.CartDetail.Include(x => x.Product).Where(x => x.CartId == cartId).ToList();
+                return View(cartDetails);
+            }   
         }
 
-        // GET: CartCustomerController/Details/5
-        public ActionResult Details(int id)
+		public ActionResult GetImage(int id)
+		{
+			var product = _context.Product.Find(id); // Thay Products bằng tên bảng sản phẩm của bạn
+			if (product != null)
+			{
+				// Lấy ảnh sản phẩm từ cơ sở dữ liệu
+				byte[] imageBytes = product.Image; // Thay Image bằng tên trường chứa ảnh trong bảng sản phẩm của bạn
+				return File(imageBytes, "image/jpeg");
+			}
+			return null;
+		}
+
+		// GET: CartCustomerController/Details/5
+		public ActionResult Details(int id)
         {
             return View();
         }
