6ba3a5df96257112b7e2dc64e120f3a0225a325d
Sir_Giap <140328785+mrgiap736@users.noreply.github.com>
2024-06-05 03:47:12 +0700

    Xong product

---
+++ DU_AN_GROUP113_NET105/Areas/Admin/Controllers/SizesController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DU_AN_GROUP113_NET105.Models.Data;
+using DU_AN_GROUP113_NET105.Models.Entities;
+
+namespace DU_AN_GROUP113_NET105.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SizesController : Controller
+    {
+        private readonly ProjectContext _context;
+
+        public SizesController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Sizes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Size.ToListAsync());
+        }
+
+        // GET: Admin/Sizes/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = await _context.Size
+                .FirstOrDefaultAsync(m => m.Name == id);
+            if (size == null)
+            {
+                return NotFound();
+            }
+
+            return View(size);
+        }
+
+        // GET: Admin/Sizes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Sizes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name")] Size size)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(size);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(size);
+        }
+
+        // GET: Admin/Sizes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = await _context.Size.FindAsync(id);
+            if (size == null)
+            {
+                return NotFound();
+            }
+            return View(size);
+        }
+
+        // POST: Admin/Sizes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Name")] Size size)
+        {
+            if (id != size.Name)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(size);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SizeExists(size.Name))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(size);
+        }
+
+        // GET: Admin/Sizes/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = await _context.Size
+                .FirstOrDefaultAsync(m => m.Name == id);
+            if (size == null)
+            {
+                return NotFound();
+            }
+
+            return View(size);
+        }
+
+        // POST: Admin/Sizes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var size = await _context.Size.FindAsync(id);
+            if (size != null)
+            {
+                _context.Size.Remove(size);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SizeExists(string id)
+        {
+            return _context.Size.Any(e => e.Name == id);
+        }
+    }
+}
---
+++ DU_AN_GROUP113_NET105/Areas/Admin/Controllers/SuppliersController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DU_AN_GROUP113_NET105.Models.Data;
+using DU_AN_GROUP113_NET105.Models.Entities;
+
+namespace DU_AN_GROUP113_NET105.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SuppliersController : Controller
+    {
+        private readonly ProjectContext _context;
+
+        public SuppliersController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Suppliers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Supplier.ToListAsync());
+        }
+
+        // GET: Admin/Suppliers/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Supplier
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        // GET: Admin/Suppliers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Suppliers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,Phone,Address")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                supplier.Id = Guid.NewGuid();
+                _context.Add(supplier);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supplier);
+        }
+
+        // GET: Admin/Suppliers/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Supplier.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Admin/Suppliers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Email,Phone,Address")] Supplier supplier)
+        {
+            if (id != supplier.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(supplier);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SupplierExists(supplier.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supplier);
+        }
+
+        // GET: Admin/Suppliers/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Supplier
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        // POST: Admin/Suppliers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var supplier = await _context.Supplier.FindAsync(id);
+            if (supplier != null)
+            {
+                _context.Supplier.Remove(supplier);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SupplierExists(Guid id)
+        {
+            return _context.Supplier.Any(e => e.Id == id);
+        }
+    }
+}
